5c58c6bf40e629132b0577e87c5fddcdb2f40bc1
hrvoje <hrvoje.cukman@gmail.com>
2024-03-11 14:19:56 +0100

    Initial commit

---
+++ example/harlowe_chat_package_example.dart
@@ -0,0 +1,6 @@
+import 'package:harlowe_chat_package/harlowe_chat_package.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/harlowe_chat_package_test.dart
@@ -0,0 +1,16 @@
+import 'package:harlowe_chat_package/harlowe_chat_package.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
