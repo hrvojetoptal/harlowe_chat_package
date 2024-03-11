a3fb52597b31a812f3799c95a47b9710aa107e61
hrvoje <hrvoje.cukman@gmail.com>
2024-03-11 15:23:39 +0100

    Export models

--- lib/harlowe_chat_package.dart
+++ lib/harlowe_chat_package.dart
@@ -4,3 +4,6 @@
 library;
 
 export 'src/harlowe_chat_package_base.dart';
+export 'src/modesl/conversation_credentials/conversation_credentials.dart';
+export 'src/modesl/conversation_message/conversation_message.dart';
+export 'src/modesl/conversation_summary/conversation_summary.dart';
